c56e3f3c1c8eb801629e768dac7aad91d970787e
nathancabigao <nathan0714@hotmail.com>
2022-10-06 10:37:19 -0400

    Implement add function

---
+++ script.js
@@ -0,0 +1,12 @@
+/**
+ * Adds two given numbers and returns the resulting sum.
+ * @example
+ * // returns 5
+ * add(2, 3);
+ * @param {number} a - a number to be added
+ * @param {number} b - a number to be added
+ * @returns {number} The sum of a and b.
+ */
+function add(a, b){
+    return a + b;
+}
